552665d19691a4b717491f3db3a33822b88df2c1
ratan <ratanchakma68@gmail.com>
2024-06-02 22:41:31 +0600

    navbar components updated and font family added

--- src/shared/Header/Navbar.jsx
+++ src/shared/Header/Navbar.jsx
@@ -1,16 +1,16 @@
-import { Link } from 'react-router-dom';
+import { NavLink,Link } from 'react-router-dom';
 import logo from '../../assets/logo/logo.png'
 import { FaPhone } from 'react-icons/fa';
 import { MdEmail } from 'react-icons/md';
 const Navbar = () => {
     const navLinks = <>
-        <Link to='/'><li className='ml-5 text-[15px] font-bold'>Home</li></Link>
+        <NavLink to='/'><li className='ml-5 text-[15px] font-bold'>Home</li></NavLink>
         <li className='ml-5 text-[15px] font-bold'>Pet Listing</li>
         <li className='ml-5 text-[15px] font-bold'>Donation Campaigns</li>
     </>
     return (
+
         <nav>
-            {/* top nav bar */}
             <section className="bg-[#1e4272] min-h-[40px] px-4 py-2 sm:px-10 flex items-center max-sm:flex-col">
                 <button type="button" className="text-white text-sm">
                     <span className="mr-3 inline-block"><FaPhone /></span>
@@ -27,20 +27,27 @@ const Navbar = () => {
                     <Link to='/register' className="text-white text-sm ml-1">Register</Link>
                 </div>
             </section>
-            {/* bottom nav bar */}
-            <div className="navbar ">
-                <div className="flex-1 gap-1">
-                    <img className='w-10 h-10' src={logo} alt="" />
-                    <p className='text-xl font-bold'>PetAdoptZone</p>
-                </div>
-                <div className="flex-none">
-                    <div className="dropdown dropdown-end">
-                        <div className="navbar-center hidden lg:flex">
-                            <ul className="menu menu-horizontal px-1">
-                                {navLinks}
-                            </ul>
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            {navLinks}
+                        </ul>
                     </div>
+                    <div className="flex gap-1 justify-center items-center">
+                        <img className='w-10 h-10' src={logo} alt="" />
+                        <p className='text-xl font-bold'>PetAdoptZone</p>
+                    </div>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {navLinks}
+                    </ul>
+                </div>
+                <div className="navbar-end">
                     <div className="dropdown dropdown-end">
                         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                             <div className="w-10 rounded-full">
@@ -48,14 +55,20 @@ const Navbar = () => {
                             </div>
                         </div>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Profile</a></li>
-                            <li><a>Dashboard</a></li>
+                            <li>
+                                <a className="justify-between">
+                                    Profile
+                                    <span className="badge">New</span>
+                                </a>
+                            </li>
+                            <li><a>Settings</a></li>
                             <li><a>Logout</a></li>
                         </ul>
                     </div>
                 </div>
             </div>
         </nav>
+
 
     );
 };
